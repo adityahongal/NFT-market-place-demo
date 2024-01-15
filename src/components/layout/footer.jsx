ba47446ebe73b9b89dda65ab505b5238f9ac82d0
aditya <adityahongalwork@gmail.com>
2024-01-15 18:06:29 +0530

    Footer to be done stack

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import { Box, useColorModeValue } from '@chakra-ui/react';
+import { Box, useColorModeValue } from "@chakra-ui/react";
+import { Stack } from "@chakra-ui/react";
 
 const Footer = () => {
-    return (
-        <Box 
-            bg={useColorModeValue("brand_pink.100","gray.900")}
-            color={useColorModeValue("gray.700","gray.200")}
-            pt={{ base: 10, md: 16}}
-            pb={{ base: 10, md: 8}}
-            px={{ base: 4, md:16}}
-        >
-        
+  return (
+    <Box
+      bg={useColorModeValue("brand_pink.100", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+      pt={{ base: 10, md: 16 }}
+      pb={{ base: 10, md: 8 }}
+      px={{ base: 4, md: 16 }}
+    >
+      <Stack>
 
-        </Box>
-    )
+      </Stack>
+    </Box>
+  );
 };
 
 export default Footer;
