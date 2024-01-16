70cf9ee110f3faad74ef213c0b0d7ab7de802008
aditya <adityahongalwork@gmail.com>
2024-01-16 18:13:13 +0530

    Aligned Footer and Nav Bar

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import NavBar from "./navbar";
 import Footer from "./footer";
 import PropTypes from "prop-types";
+import { VStack, Box } from "@chakra-ui/react";
 
 const Layout = ({ children }) => {
     return(
-        <>
+        <VStack spacing={0} align={"start"} minH={"100vh"}>
+        <Box width={"full"}>
         <NavBar/>
+        </Box>
+        <VStack spacing={0} align={"start"} flex={1}>
         {children}
-        <Footer/>
-        </>
+        </VStack>
+       <Box width={"full"}>
+       <Footer/>
+       </Box>
+        </VStack>
     )
 }
 
